fbf945dfddd94924085368e4c3f7fb7d9dfdc8a0
erickuu <erickgraterol377@gmail.com>
2022-01-28 19:33:33 +0100

    Project on php

---
+++ index.php
@@ -0,0 +1,70 @@
+<?php include("db.php")?>
+<?php include("includes/header.php")?>
+
+    <div class="container p-4">
+
+        <div class="row">
+            <div class="col-md-4">
+                <?php if(isset($_SESSION['message'])){?>
+                    <div class="alert alert-<?= $_SESSION['message_type'];?> s alert-dismissible fade show" role="alert">
+                        <?= $_SESSION['message']?>
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
+                        </button>
+                    </div>
+                    <?php session_unset();}?>
+                <div class="card card-body">
+                    <form action="save_task.php" method="POST"> 
+                        <div class="form-group">
+                            <input type="text" name="title" class="mt-3 form-control" placeholder="Escribe tu tarea" autofocus>
+                        </div>
+                        <div class="form-group ">
+                            <textarea name="description" id="" rows="5" class="mt-3 form-control" placeholder="Descripcion"></textarea>
+                        </div>
+                        <input type="submit" class="mt-3 btn btn-success btn-block" name="save_task" value="Guardar">  
+                    </form>
+                </div>
+            </div>
+            <div class="col-md-8">
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th>Titulo</th>
+                            <th>Descripcion</th>
+                            <th>Fecha De creacion</th>
+                            <th>Acciones</th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                            <?php
+                            $query = "SELECT * FROM task";
+                            $resultSql = mysqli_query($conn, $query);
+                            
+                            while($getRow = mysqli_fetch_array($resultSql))
+                            {?>
+                            <tr>
+                                <td><?php echo $getRow['title']?></td>
+                                <td><?php echo $getRow['description']?></td>
+                                <td><?php echo $getRow['create_at']?></td>
+                                <td>
+                                    <a class="icons btn btn-danger" style="color:brownie;" href="delete_task.php?id=<?php print($getRow['id']); ?>">
+                                        <i class="far fa-trash-alt"></i>
+                                    </a>
+                                    <span></span>   
+                                    <a class="icons btn btn-warning" style="color:white;" href="edit.php?id=<?php print($getRow['id']); ?>">
+                                        <i class="fas fa-marker"></i>
+                                    </a>
+                                </td>
+                            </tr>
+                            <?php } ?>
+        
+                            
+                        
+                    </tbody>
+
+                </table>
+            </div>
+        </div>
+    </div>
+
+<?php include("includes/footer.php")?>
